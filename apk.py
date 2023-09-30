c607343a89e61e470e53c18358c6052c3e4926fc
active <66892517+activ33@users.noreply.github.com>
2023-09-30 20:35:43 +0530

    Update apk.py

--- apk.py
+++ apk.py
@@ -22,7 +22,7 @@ async def pahe(bot,msg):
   global url
   search_url = url + "api?m=search&q=" + query
   response = session.get(search_url)
-  msg.reply(response.text.split(",",1)[0])
+  await msg.reply(response.text.split(",",1)[0])
 
 app.run()
   
