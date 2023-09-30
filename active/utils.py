5a6cb43dc5f33247a85d7708c5c05575034a39fb
active <66892517+activ33@users.noreply.github.com>
2023-09-30 16:32:23 +0530

    Update utils.py

--- active/utils.py
+++ active/utils.py
@@ -32,8 +32,7 @@ async def search_apahe(query: str) -> list:
     global url
     search_url = url + "api?m=search&q=" + query
     response = session.get(search_url)
-    data = response.text
-    await Client.send_message(chat_id=689061386,text=f"Text : {data}")
+    data = response.json()
     clean_data = []
     for i in data["data"]:
         hmm = []
