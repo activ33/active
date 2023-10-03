fa6b71f545ebd7455804db56faeae959db7ab7ca
active <66892517+activ33@users.noreply.github.com>
2023-10-04 02:40:29 +0530

    Update heroe.py

--- active/heroe.py
+++ active/heroe.py
@@ -23,19 +23,14 @@ async def nonee(bot,msg):
   lang = "jpn"
   quality = int(hero[4])
   list_of_anime = pahe.search_apahe(query)
-  host = await msg.reply(list_of_anime)
   anime_id = list_of_anime[choice][6]
-  await host.edit_text(anime_id)
   episode_range = (
     [int(episode_range[0]), int(episode_range[1]) + 1]
     if len(episode_range) == 2
     else [int(episode_range[0]), int(episode_range[0]) + 1]
 )
-  await host.edit_text(episode_range)
   episode_ids = pahe.mid_apahe(session_id=anime_id, episode_range=episode_range)
-  await host.edit_text(episode_ids)
   episodes_data = pahe.dl_apahe1(anime_id=anime_id, episode_ids=episode_ids)
-  await host.edit_text(episodes_data)
   episodes = {}
   index = episode_range[0]
   for key, value in episodes_data.items():
@@ -71,9 +66,9 @@ async def nonee(bot,msg):
     destination = os.path.join("Anime",f"{key} - {title} [{quality}p] [ESH] @AnimeFiles.mkv")
     download_link = get_dl_link(value)
     print("download_link")
-    sts = await msg.reply("Downloading Started")
+    sts = await msg.reply(f"Downloading Started | {key} - {ttitle} {quality}")
     c_time = time.time()
     anime = pahe.download_file(url=download_link, destination=destination)
-    upl = await msg.reply_document(document=destination,progress=progress_message, progress_args=("Upload Started.....", sts, c_time))
+    upl = await msg.reply_document(document=destination,thumb="Thumb.jpg",progress=progress_message, progress_args=("Upload Started.....", sts, c_time))
     await sts.delete()
     os.remove(destination)
