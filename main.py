d2976ebf2865420cc37d407d260c1e0b3c51b58d
active <66892517+activ33@users.noreply.github.com>
2023-10-03 23:08:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from pyrogram import Client
 plugins = dict(root="active")
 api = 24541704
 hash = "7b1b63a5c5a2e53233a0e78727c068d3"
-token = "6649077119:AAEfm6xUWnqE06SlGQDjqm6qt3OO6Ki4v_Y"
+token = "6523972674:AAFsskflpvAFrvhn3IP-Q0645nhgq-gFzFI"
 
 print("⚡ Starting...")
 Client("Active", api_id=api, api_hash=hash, bot_token=token,
