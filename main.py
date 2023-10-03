d3fbe2a4bd447ff97e11d59c3b666a87bc118719
active <66892517+activ33@users.noreply.github.com>
2023-10-04 02:46:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+from os import environ
 from pyrogram import Client
 
 plugins = dict(root="active")
 api = 24541704
 hash = "7b1b63a5c5a2e53233a0e78727c068d3"
-token = "6523972674:AAFsskflpvAFrvhn3IP-Q0645nhgq-gFzFI"
+token = environ.ge("token")
 
 print("⚡ Starting...")
 Client("Active", api_id=api, api_hash=hash, bot_token=token,
