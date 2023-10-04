fd44530d7c757c6c91aeaa14e5d348443b01d92c
active <66892517+activ33@users.noreply.github.com>
2023-10-04 15:38:55 +0530

    Update post.py

--- active/post.py
+++ active/post.py
@@ -1,21 +1,7 @@
 from pyrogram import Client,filters
-from requests_html import HTMLSession
-s = HTMLSession()
 
 @Client.on_message(filters.command("post"))
 async def post(bot,msg):
-  umi = "https://animepahe.ru/"
-  query = msg.text.split(" ",1)[1]
-  search_url = umi + "api?m=search&q=" + query
-  response = s.get(search_url)
-  amir = response.text
-  f = open("new.txt", "w")
-  f.write(amir)
-  f.close()
-  await msg.reply_document("new.txt")
-
-@Client.on_message(filters.command("hero")
-async def hero(bot,msg):
   await msg.reply_document("new.txt")
                      
   
