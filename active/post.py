064e671177ec5630cfdda86597864cb23baf96d5
active <66892517+activ33@users.noreply.github.com>
2023-10-04 15:05:53 +0530

    Update post.py

--- active/post.py
+++ active/post.py
@@ -1,18 +1,12 @@
 from pyrogram import Client,filters
-import requests
-from telegraph import Telegraph
-from requests_html import HTMLSession
-rqq = HTMLSession()
-s = requests.Session()
-telegraph = Telegraph()
-telegraph.create_account(short_name='1337')
+import httpx
 
 @Client.on_message(filters.command("post"))
 async def post(bot,msg):
   umi = "https://animepahe.ru/"
   query = msg.text.split(" ",1)[1]
   search_url = umi + "api?m=search&q=" + query
-  response = s.get(search_url)
+  response = httpx.get(search_url)
   amir = response.text
   f = open("new.txt", "w")
   f.write(amir)
